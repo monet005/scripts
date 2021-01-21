57dffb67b17740a30fc2fb93ad7de675ecbc74e2
monet005 <monet.velasco@gmail.com>
2021-01-21 09:09:13 +0800

    updated test_systemd.py

--- test_systemd.py
+++ test_systemd.py
@@ -3,6 +3,7 @@
 import requests
 import logging
 import sys
+import os
 
 
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
@@ -10,6 +11,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
 logger = logging.getLogger(__name__)
 
 consul_url = 'http://pi-1:8500'
+systemd_dirname = 'sabre_systemd'
 
 
 class ConsulHelper():
@@ -28,12 +30,15 @@ class ConsulHelper():
                              .format(self.kv_url, service_name))
             if r.status_code == 200:
                 data = r.text
+                logger.info('{} systemd config download OK'
+                            .format(service_name))
                 return data
             else:
-                logger.warning('{} does not exist in consul key-value store'
+                logger.warning('{} service does not have consul '
+                               'systemd key-value entry'
                                .format(service_name))
         except Exception as e:
-            logger.error('Error connecting to {}'.format(consul_url))
+            logger.error('Error connecting to {}: {}'.format(consul_url, e))
             sys.exit(1)
 
     def get_all_services(self):
@@ -45,7 +50,7 @@ class ConsulHelper():
             else:
                 logger.warning('Error in getting data from consul')
         except Exception as e:
-            logger.error('Error connecting to {}'.format(consul_url))
+            logger.error('Error connecting to {}: {}'.format(consul_url, e))
             sys.exit(1)
 
         return services
@@ -63,7 +68,7 @@ class ConsulHelper():
                 logger.warning('Error in getting data from consul')
 
         except Exception as e:
-            logger.error('Error connecting to {}'.format(consul_url))
+            logger.error('Unable to connect to {}: {}'.format(consul_url, e))
             sys.exit(1)
 
         return svc_node_map
@@ -71,25 +76,31 @@ class ConsulHelper():
     def get_systemd_status_per_node(self, service_name):
         status_map = {}
         try:
-            print('{}/service/{}'
-                  .format(self.ctlg_url, service_name))
             r = requests.get('{}/service/{}'
                              .format(self.ctlg_url, service_name))
-            print(r.status_code)
             items = r.json()
             if items:
                 status_items = []
                 for item in items:
-                    status = item.get('ServiceMeta')['systemd_enabled']
                     node = item.get('Node')
-                    status_items.append({'node': node,
-                                         'systemd_enabled': status})
+                    try:
+                        status = item.get('ServiceMeta')['systemd_enabled']
+                        if status:
+                            status_items.append({'node': node,
+                                                 'systemd_enabled': status})
+                        else:
+                            logger.warning('{} - systemd_enabled key is null'
+                                           .format(service_name))
+                    except KeyError:
+                        logger.warning('{} service json file on {} '
+                                       'does not have systemd_enabled key'
+                                       .format(service_name, node))
                 status_map.update({service_name: status_items})
             else:
                 logger.warning('{} service does not exist in consul'
                                .format(service_name))
         except Exception as e:
-            logger.error('Error connecting to {}'.format(consul_url))
+            logger.error('Error connecting to {}: {}'.format(consul_url))
             sys.exit(1)
 
         return status_map
@@ -104,28 +115,45 @@ class ConsulHelper():
         return all
 
 
-
-
-
-
-
 def main():
     ch = ConsulHelper(consul_url)
+    all_services = ch.get_all_services()
 
-    a = ch.get_all_nodes()
-    b = ch.get_systemd_status_per_node('jenkins')
-    print(b)
+    # Systemd and ansible inventory file creation tasks
+    try:
+        os.mkdir(systemd_dirname)
+    except Exception as e:
+        logger.warning('Unable to create {} directory: {}'
+                       .format(systemd_dirname, e))
 
+    try:
+        os.remove('systemd_enabled_hosts')
+        logger.info('systemd_enabled_hosts cleared')
+    except Exception as e:
+        logger.error('Unable to delete systemd_enabled_hosts: {}'
+                     .format(e))
 
+    for svc_name in all_services:
+        svc_data = ch.get_systemd_config(svc_name)
+        if svc_data:
+            dest = '{}/{}.service'.format(systemd_dirname, svc_name)
+            with open(dest, 'w') as f:
+                f.write(svc_data)
+                logger.info('{} systemd template created'.format(dest))
 
+            svc_status_map = ch.get_systemd_status_per_node(svc_name)
 
+            with open('systemd_enabled_hosts', 'a') as f:
+                if svc_status_map.get(svc_name):
+                    f.write('\n')
+                    f.write('[{}]\n'.format(svc_name))
 
+                    values = svc_status_map.get(svc_name)
+                    for v in values:
+                        if v.get('systemd_enabled') == 'true':
+                            f.write('{}\n'.format(v.get('node')))
+                            
 
 
 if __name__ == "__main__":
     main()
-
-
-   
-
-

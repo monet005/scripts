2836ae40b14d76383db6e71eae507320c665f29b
monet005 <monet.velasco@gmail.com>
2021-02-07 16:08:13 +0800

    updated systemd_handler.sh

--- systemd_handler.sh
+++ systemd_handler.sh
@@ -3,7 +3,8 @@
 SYSTEMD_DIR=$HOME/.config/systemd/user
 KV_PREFIX=systemd
 KEY=$1
-SVC_NAME=${KEY}.service
+SVC_KEY=$(echo $1 | tr ' ', '-')
+SVC_NAME=${SVC_KEY}.service
 SVC_TMP=/tmp/${SVC_NAME}
 SVC_USER=${SYSTEMD_DIR}/${SVC_NAME}
 
@@ -17,14 +18,14 @@ logit () {
 
 [ -d $SYSTEMD_DIR ] || mkdir -p $SYSTEMD_DIR
 
-consul kv get $KV_PREFIX/${KEY} > $SVC_TMP && \
+consul kv get $KV_PREFIX/"${KEY}" > $SVC_TMP && \
 logit info "$SVC_NAME change detected"
 
 sudo systemd-analyze verify $SVC_TMP >/dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     mv $SVC_TMP $SVC_USER
     systemctl --user daemon-reload
-    systemctl --user enable
+    systemctl --user enable $SVC_NAME
     CHECK=$(systemctl --user is-active $SVC_NAME)
     if [[ $CHECK != 'active' ]]; then
         systemctl --user start $SVC_NAME && \

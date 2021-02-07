861aa57c2565758feb851ba826389cce029a201a
monet005 <monet.velasco@gmail.com>
2021-02-07 12:28:31 +0800

    udpated systemd_handler.sh

--- systemd_handler.sh
+++ systemd_handler.sh
@@ -2,21 +2,37 @@
 
 SYSTEMD_DIR=$HOME/.config/systemd/user
 KV_PREFIX=systemd
-SVC_NAME=$1
+KEY=$1
+SVC_NAME=${KEY}.service
+SVC_TMP=/tmp/${SVC_NAME}
+SVC_USER=${SYSTEMD_DIR}/${SVC_NAME}
+
+
+logit () {
+  case "$1" in
+    info) echo "$2" | systemd-cat -t sabre-systemd -p info ;;
+    warn) echo "$2" | systemd-cat -t sabre-systemd -p warning ;;
+  esac
+}
 
 [ -d $SYSTEMD_DIR ] || mkdir -p $SYSTEMD_DIR
 
-consul kv get $KV_PREFIX/${SVC_NAME} > ${SYSTEMD_DIR}/${SVC_NAME}.service
+consul kv get $KV_PREFIX/${KEY} > $SVC_TMP && \
+logit info "$SVC_NAME change detected"
 
-sudo systemd-analyze verify ${SYSTEMD_DIR}/${SVC_NAME}.service >/dev/null 2>&1
+sudo systemd-analyze verify $SVC_TMP >/dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    echo "${SVC_NAME}.service added" | systemd-cat -t sabre-systemd -p info
+    mv $SVC_TMP $SVC_USER
     systemctl --user daemon-reload
-    systemctl --user start ${SVC_NAME}.service && \
-    echo "${SVC_NAME}.service started" | systemd-cat -t sabre-systemd -p info
+    systemctl --user enable
+    CHECK=$(systemctl --user is-active $SVC_NAME)
+    if [[ $CHECK != 'active' ]]; then
+        systemctl --user start $SVC_NAME && \
+        logit info "$SVC_NAME started"
+    else
+        logit info "$SVC_NAME already running"
+    fi
 else
-    echo "${SVC_NAME}.service - systemd configuration checks failed" \
-    | systemd-cat -t sabre-systemd -p info
-    rm -f ${SYSTEMD_DIR}/${SVC_NAME}.service
-    systemctl --user daemon-reload
+    logit warn "$SVC_NAME config checks failed, not updating the service"
+    rm -f $SVC_TMP
 fi
